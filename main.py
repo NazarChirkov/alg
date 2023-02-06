04d5a3a7d18183adda77e41437da35c2477b949e
NazarChirkov <nazarchirkov@icloud.com>
2023-02-06 18:38:31 +0000

    linked list

--- main.py
+++ main.py
@@ -228,3 +228,34 @@ d.addFront(55)
 d.addRear(45)
 print(d.items)
 '''
+# Linked list implementation in Python
+
+
+class Node:
+    # Creating a node
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+
+if __name__ == '__main__':
+
+
+    linked_list = LinkedList()
+
+    linked_list.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+
+    linked_list.head.next = second
+    second.next = third
+
+    while linked_list.head != None:
+        print(linked_list.head.item, end = " ")
+        linked_list.head = linked_list.head.next
